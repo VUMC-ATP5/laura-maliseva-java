135728816f50b894ac5354523aefb72e6d734b29
maliseva <112900356+maliseva@users.noreply.github.com>
2022-11-02 19:42:19 +0200

    majas darbs nr. 5

--- src/main/java/lekcijaPieci/majasDarbs/Main.java
+++ src/main/java/lekcijaPieci/majasDarbs/Main.java
@@ -1,13 +1,29 @@
 package lekcijaPieci.majasDarbs;
 
-import lekcijaCetri.labDarbs.Rinkis;
-import lekcijaPieci.labDarbs.Dog;
-
 public class Main {
+
     public static void main(String[] args) {
-        Dog suns = new Dog();
+
+        Cilindrs cilindrsViens = new Cilindrs();
+        cilindrsViens.setAugstums(4);
+        cilindrsViens.setRadiuss(5);
+
+        Cilindrs cilindsDivi = new Cilindrs(3, 4);
+
+        System.out.println("Pirmā cilindra augstums un radiuss: " + cilindrsViens.getAugstums() + ", " + cilindrsViens.getRadiuss());
+        System.out.println("Otrā cilindra augstums un radiuss: " +  cilindsDivi.getAugstums() + ", " + cilindsDivi.getRadiuss());
+
+        System.out.println("Pirmā cilindra tilpums: " + cilindrsViens.rekinatCilindraTilpums());
+        System.out.println("Pirmā cilindra virsmas laukums: " + cilindrsViens.rekinatCilindraVirsmasLaukumu());
+
+        System.out.println("Otrā cilindra tilpums: " + cilindsDivi.rekinatCilindraTilpums());
+        System.out.println("Otrā cilindra virsmas laukums: " + cilindsDivi.rekinatCilindraVirsmasLaukumu());
+
+
+
 
 
 
     }
+
 }
